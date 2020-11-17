2e6002ba1d4eb58baf48a6d8f285371c7500c6cb
Shirly R <shirly.rabin@otorio.com>
2020-11-17 11:54:09 +0200

    edited test with array access in while

---
+++ src/checked_tests/var/formal_param_with_while_and_assign_statements.java
@@ -0,0 +1,45 @@
+//class Main {
+//	public static void main(String[] args) {
+//		System.out.println(1);
+//	}
+//}
+//
+//class A {
+//}
+//
+//class B extends A {
+//	int theVar;
+//
+//	int foo() {
+//		return theVar;
+//	}
+//
+//}
+//
+//class C extends A {
+//	int theVar;
+//
+//	int foo() {
+//		return theVar;
+//	}
+//
+//}
+//
+//class D extends C {
+//	int[] maxArr;
+//
+//	int bar(int theVar, int anotherVar) {
+//		int max;
+//		while (!((maxArr)[theVar])) {			{
+//				theVar = (theVar) + (anotherVar);
+//
+//				max = (2) * (theVar);
+//
+//			}
+//
+//		}
+//		return max;
+//	}
+//
+//}
+//
